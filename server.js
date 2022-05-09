146e02539d6a65c3e17c118dec881923b3aceb90
Temirlan <Dosmailovtemirlan@gmail.com>
2022-05-09 18:21:11 +0600

    new

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const app = express();
-const port = 3000;
+// const port = 3000;
 app.set("views engine", "ejs");
 app.use(express.static("assets"));
 app.use("/", require("./routes/root"));
@@ -9,10 +9,13 @@ app.use("/about", require("./routes/about"));
 app.use("/genre", require("./routes/genre"));
 app.use("/index", require("./routes/index"));
 app.use("/contact",require("./routes/contact"));
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 )
-
 
 
 
